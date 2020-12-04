3ede0c2d494121a1097e7612326a05428e9d064e
Jacob Stephens <jacobstephens2@gmail.com>
2020-12-03 17:53:59 -0800

    added followers array

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,8 +5,8 @@ https://api.github.com/users/<your name>
 */
 import axios from 'axios';
 const axiosData = axios.get('https://api.github.com/users/jacobstephens17');
-console.log(axiosData)
-const entry = document.querySelector('.card');
+
+const entryPoint = document.querySelectorAll('.cards');
 
 /*
 STEP 2: Inspect and study the data coming back, this is YOUR
@@ -32,7 +32,17 @@ Using that array, iterate over it, requesting data for each user, creating a new
 user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetonda', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach((item) =>{
+  axios
+    .get('https://api.github.com/users/' + item)
+    .then(res =>{
+      newOne = cardMaker(res.data);
+      entry.appendChild(newOne);
+    })
+
+})
 
 /*
 STEP 3: Create a function that accepts a single object as its only argument.
@@ -76,7 +86,7 @@ function cardMaker (obj){
   following.textContent = `Following: ${obj.data.following}`;
   bio.textContent = `Bio: ${obj.data.bio}`;
   
-  card.classList.add('card');
+  card.classList.add('cards');
   cardInfo.classList.add('card-info');
   name.className.add('name');
   userName.classList.add('username');
@@ -92,7 +102,7 @@ function cardMaker (obj){
   cardInfo.appendChild(following);
   cardInfo.appendChild(bio);
   
-  return card;
+  return card
 } 
 /*
 List of LS Instructors Github username's:
@@ -104,10 +114,11 @@ bigknell
 */
 
 axios
-.get('https://api.github.com/users/jacobstephens17')
-.then((res) =>{
-  const dataRec = res.data;
-  const newUsers = cardMaker(dataRec);
-  entry.appendChild(newUsers); 
-})
+    .get('https://api.github.com/users/jacobstephens17')
+      .then((res) =>{
+      const dataRec = res.data;
+      const newUsers = cardMaker(dataRec);
+      entry.appendChild(newUsers);    
+});
+
 
