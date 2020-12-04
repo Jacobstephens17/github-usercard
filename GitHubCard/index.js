4bc61ae09160818c23c0ed02f67abf07c9890f06
Jacob Stephens <jacobstephens2@gmail.com>
2020-12-03 16:43:15 -0800

    got the picture to change but in the wrong place, i belive i have everything correct except for the fact that it isnt appending to the page

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,72 +5,109 @@
 */
 import axios from 'axios';
 const axiosData = axios.get('https://api.github.com/users/jacobstephens17');
-
 console.log(axiosData)
 
 /*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
+STEP 2: Inspect and study the data coming back, this is YOUR
+github info! You will need to understand the structure of this
+data in order to use it to build your component function
 
-    Skip to STEP 3.
+Skip to STEP 3.
 */
 
 /*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
+STEP 4: Pass the data received from Github into your function,
+and append the returned markup to the DOM as a child of .cards
 */
 
 /*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
+STEP 5: Now that you have your own card getting added to the DOM, either
+follow this link in your browser https://api.github.com/users/<Your github name>/followers,
+manually find some other users' github handles, or use the list found at the
+bottom of the page. Get at least 5 different Github usernames and add them as
+Individual strings to the friendsArray below.
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
+Using that array, iterate over it, requesting data for each user, creating a new card for each
+user, and adding that card to the DOM.
 */
 
 const followersArray = [];
 
 /*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
+STEP 3: Create a function that accepts a single object as its only argument.
+Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
+<div class="card">
+<img src={image url of user} />
+<div class="card-info">
+<h3 class="name">{users name}</h3>
+<p class="username">{users user name}</p>
+<p>Location: {users location}</p>
+<p>Profile:
+<a href={address to users github page}>{address to users github page}</a>
+</p>
+<p>Followers: {users followers count}</p>
+<p>Following: {users following count}</p>
+<p>Bio: {users bio}</p>
+</div>
+</div>
 */
-const cardMaker = function(obj){
-  const card = document.querySelector('.card');
-  const image = document.querySelector('img');
+function cardMaker (obj){
+  const card = document.querySelector('div');
+  const profilePic = document.querySelector('img');
   const cardInfo = document.querySelector('div');
   const name = document.querySelector('h3');
   const userName = document.querySelector('p');
   const location = document.querySelector('p');
   const profile = document.querySelector('p');
   const profileAddress = document.querySelector('a');
-  const followers = document.querySelector('{followers}');
-  const following = document.querySelector('{following}');
-  const bio = document.querySelector('{bio}')
-}
+  const followers = document.querySelector('p');
+  const following = document.querySelector('p');
+  const bio = document.querySelector('p')
+  
+  profilePic.src = obj.data.avatar_url;
+  name.textContent = obj.data.name;
+  userName.textContent = obj.data.login;
+  location.textContent = `Location: ${obj.data.location}`;
+  profile.textContent = "Profile:";
+  profileLink.href = obj.data.html_url;
+  followers.textContent = `Followers: ${obj.data.followers}`;
+  following.textContent = `Following: ${obj.data.following}`;
+  bio.textContent = `Bio: ${obj.data.bio}`;
+  
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.className.add('name');
+  userName.classList.add('username');
+  
+  card.appendChild(profilePic);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(profileAddress);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  
+  return card;
+} 
 /*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
+List of LS Instructors Github username's:
+tetondan
+dustinmyers
+justsml
+luishrd
+bigknell
 */
+const entry = document.querySelector('.card');
+
+axios
+.get('https://api.github.com/users/jacobstephens17')
+.then((res) =>{
+  const accData = res.data
+  const newUsers = cardMaker(res);
+  entry.append(newUsers);    
+})
+
